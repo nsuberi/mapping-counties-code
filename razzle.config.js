962d7440792dc8ace9fea669fe0bdca502a344fc
nsuberi <nsuberi@gmail.com>
2018-12-19 20:05:31 -0500

    attempt to use razzle-heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,16 +1,19 @@
+/* modify: (config, { target, dev }, webpack) => {
+
+    require('razzle-heroku')
+
+    // load webfonts
+    rules = config.module.rules || [];
+    rules.push({
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]'
+    })
+    config.module.rules = rules;
+
+    return config;
+},
+*/
+
 module.exports = {
-    modify: (config, { target, dev }, webpack) => {
-
-        require('razzle-heroku')
-
-        // load webfonts
-        rules = config.module.rules || [];
-        rules.push({
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=public/fonts/[name].[ext]'
-        })
-        config.module.rules = rules;
-
-        return config;
-    },
+    modify: require('razzle-heroku')
 };
