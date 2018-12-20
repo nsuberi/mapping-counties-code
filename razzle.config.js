275ace487c2c8bbdc8bdfd2611c11cbeca7c6567
nsuberi <nsuberi@gmail.com>
2018-12-19 20:26:03 -0500

    revert to webpack extending version of the razzle.config.js

--- razzle.config.js
+++ razzle.config.js
@@ -1,19 +1,16 @@
-/* modify: (config, { target, dev }, webpack) => {
-
-    require('razzle-heroku')
-
-    // load webfonts
-    rules = config.module.rules || [];
-    rules.push({
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=public/fonts/[name].[ext]'
-    })
-    config.module.rules = rules;
-
-    return config;
-},
-*/
-
 module.exports = {
-  modify: require("razzle-heroku")
+  modify: modify: (config, { target, dev }, webpack) => {
+
+      require('razzle-heroku')
+
+      // load webfonts
+      rules = config.module.rules || [];
+      rules.push({
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          loader: 'file-loader?name=public/fonts/[name].[ext]'
+      })
+      config.module.rules = rules;
+
+      return config;
+  },
 };
